70dd9b697d54dbb9eff794f9dd7dd1a8c573d8c8
rutebarbalho <rute-barbalho@outlook.com>
2016-11-08 14:51:35 -0200

    ProjetoGrupo6

---
+++ ProjetoGrupo6/PaginaFilme.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PaginaFilme.aspx.cs" Inherits="ProjetoGrupo6.PaginaFilme" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #TextArea1 {
+            height: 79px;
+            width: 268px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="nomedofilme" runat="server" Text="Nome Do Filme"></asp:Label>
+        <br />
+
+    </div>
+        <br />
+        <asp:TextBox ID="TextBoxComentario" runat="server" Height="63px" Width="275px" OnTextChanged="TextBoxComentario_TextChanged"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    </form>
+</body>
+</html>
---
+++ ProjetoGrupo6/PaginaFilme.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjetoGrupo6
+{
+    public partial class PaginaFilme : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string aSQLConecStr;
+
+            // Lendo a conexão de dados do Web.Config
+            aSQLConecStr = ConfigurationManager.ConnectionStrings["2016TiiGrupo6ConnectionString"].ConnectionString;
+
+            // Abrindo a Conexão com o banco de dados
+            SqlConnection aSQLCon = new SqlConnection(aSQLConecStr);
+            aSQLCon.Open();
+
+            // Executando o comando
+            SqlCommand aSQL = new SqlCommand("INSERT INTO comentario(descricao) VALUES (@descricao)", aSQLCon);
+            aSQL.Parameters.AddWithValue("@descricao", TextBoxComentario.Text);
+          
+            aSQL.ExecuteNonQuery();
+        }
+
+        protected void TextBoxComentario_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
